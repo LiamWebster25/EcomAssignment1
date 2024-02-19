a473304544ac4a8bc5dd1db54df3600096c7f472
Hadi Sasa <2291195@edu.vaniercollege.qc.ca>
2024-02-19 15:34:00 -0500

    landing page done

---
+++ app/views/Person/Contact/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site Landing</title>
+</head>
+<body>
+    <div>
+        <nav>
+            <ul>
+                <a href="#"><li>Landing Page</li></a>
+                <a href="#"><li>About Us</li></a>
+                <a href="#"><li>Contact Us</li></a>
+                <a href="#"><li>See the messages we get</li></a>
+            </ul>
+        </nav>
+    </div>
+    <div>
+        <h1>Landing Page</h1>
+        <p>Welcome to this Web application.</p>
+        <p>We aim to please.</p>
+    </div>
+</body>
+</html>
+
+<style>
+    body {
+        display: flex;
+        flex-direction: row;
+    }
+    nav {
+        margin-top: 20%;
+        padding-right: 10%;
+    }
+</style>
