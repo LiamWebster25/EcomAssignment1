d21e746c196f0b64585d14e75436457929c75307
Liam Webster <2265137@edu.vaniercollege.qc.ca>
2024-02-20 15:14:45 -0500

    Counter construct done

--- app/models/Counter.php
+++ app/models/Counter.php
@@ -1,6 +1,7 @@
 <?php
 
-$count;
+class Counter {
+public $count;
 
 public function __construct() {
     //file name
@@ -8,21 +9,29 @@ public function __construct() {
     //Checking to see if counter.txt file exists
     if (file_exists($fileName)) {
         //open the file
-        $file_handle = fopen($fileName, 'a');
+        $file_handle = fopen($fileName, 'r');
         //Locking the file
-        flock($file_handle, LOCK_EX);
-        
+        flock($file_handle, LOCK_SH);
+        //retrieving the information from the file
+        $this->count = fread($file_handle, filesize($fileName));
+        //close the file
+        fclose($file_handle);
+    } else {
+        $this->count = '{"count":0}';
     }
+        $this->count = json_decode($this->count)->count;
 }
 
 public function increment() {
-
+    $this->count++;
 }
 
 public function write() {
-
+    
 }
 
 public function __toString() {
+    return json_encode($this);
+}
 
 }
